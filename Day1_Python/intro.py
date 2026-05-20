56b2baf950f58a51d6c083908a4ea652ac97b86d
SaraswatiTech-tech <saraswatishinge1234@gmail.com>
2026-05-20 22:47:00 +0530

    Added Day1 Python notes

---
+++ Day1_Python/intro.py
@@ -0,0 +1,9 @@
+print("Welcome to python programming")
+age= 18
+if age >= 18:
+    print("you are eligible to vote")
+else:
+    print("you are not eligible for voting")
+
+age = 30
+print(type(age))
