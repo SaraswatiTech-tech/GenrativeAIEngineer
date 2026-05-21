65ab8831271594e4a34c4cc9dc84a80f2b6464e5
SaraswatiTech-tech <saraswatishinge1234@gmail.com>
2026-05-21 17:26:54 +0530

    Added Day 2 Python basics

--- Day2_Pyhon/variabledatatype.py
+++ Day2_Pyhon/variabledatatype.py
@@ -1,7 +1,21 @@
 Account_HolderName = input("Enter Holder Name:")
 Account_Number = int(input("Enter Account Number:"))
 Account_Balance = float(input("Enter Account Balance:"))
-is_Account_Active = True
+is_Account_Active = False
 deposit_amount = float(input("Enter Deposit Amount: "))
+
 Account_Balance = Account_Balance + deposit_amount
+
 print(Account_Balance)
+
+
+if is_Account_Active:
+    withdrawal_amount = float(input("Enter withdrawal Amount:"))
+
+    if Account_Balance >= withdrawal_amount:
+        Account_Balance = Account_Balance - withdrawal_amount
+        print("Withdrawal successful. Updated Balance:", Account_Balance)
+    else:
+        print("Insufficient balance for withdrawal.")
+else:
+    print("Account is not active. Cannot perform withdrawal.")
