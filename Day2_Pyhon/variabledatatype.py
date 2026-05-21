fd9752214d90c70fcd47aedf44b482905ae5939d
SaraswatiTech-tech <saraswatishinge1234@gmail.com>
2026-05-21 14:35:09 +0530

    Added Day 2 Python basics

--- Day2_Pyhon/variabledatatype.py
+++ Day2_Pyhon/variabledatatype.py
@@ -1,28 +1,7 @@
-Account_Holder = "Saraswati"
-Age = 22
-print("Account Holder Name: ", Account_Holder)
-print("Account Holder Age: ", Age)
-
-
-Name = Middle_Name = Last_Name = "Chukki"
-print("Name: ", Name)
-
-print("Last Name: ", Last_Name)     
-
-Student_Name = "Chukki"
-Student_Name = "Saraswati"
-
-Student_Age = 22
-Student_Last_Name = "A"
-Student_Marks = 85.5
-Student_PhoneNumber = 8296250284
-print("Student Name: ", Student_Name)
-print("Student Age: ", Student_Age)
-print("Student Last Name: ", Student_Last_Name)
-print("Student Marks: ", Student_Marks)
-print("Student Phone Number: ", Student_PhoneNumber)
-print(type(Student_Name))
-print(type(Student_Age))
-print(type(Student_Marks))
-print(type(Student_PhoneNumber))
-print(type(Student_Last_Name))
+Account_HolderName = input("Enter Holder Name:")
+Account_Number = int(input("Enter Account Number:"))
+Account_Balance = float(input("Enter Account Balance:"))
+is_Account_Active = True
+deposit_amount = float(input("Enter Deposit Amount: "))
+Account_Balance = Account_Balance + deposit_amount
+print(Account_Balance)
